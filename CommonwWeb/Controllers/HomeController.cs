3388bea314bc326101435684a72ce346286942fe
Yoshi <h.yoshikazu0110@gmail.com>
2022-08-12 08:20:56 +0900

    xmlhttpオブジェクトを利用したPDFダウンロード非同期処理の実装

--- CommonwWeb/Controllers/HomeController.cs
+++ CommonwWeb/Controllers/HomeController.cs
@@ -53,24 +53,6 @@ namespace CommonwWeb.Controllers
             //データベース読み取り
             Hashtable ret = new Hashtable();
             byte[] readImage = sMssql.ReadImage(2, ref ret);
-
-            //text/plain                 テキストファイル
-            //text/csv                    CSVファイル
-            //text/html                   HTMLファイル
-            //text/css                    CSSファイル
-            //text/javascript             JavaScriptファイル
-            //application/octet-stream    EXEファイルなどの実行ファイル
-            //application/json            JSONファイル
-            //application/pdf             PDFファイル
-            //application/vnd.ms-excel    EXCELファイル( .xls OFFICE 2007より過去)
-            //application/vnd.openxmlformats-officedocument.spreadsheetml.sheet   EXCELファイル( .xlsx OFFICE 2007以降)
-            //application/vnd.ms-powerpoint   PowerPointファイル( .ppt OFFICE 2007より過去)
-            //application/vnd.openxmlformats-officedocument.presentationml.presentation   PowerPointファイル( .pptx OFFICE 2007以降)
-            //application/msword          WORDファイル( .doc OFFICE 2007より過去)
-            //application/vnd.openxmlformats-officedocument.wordprocessingml.document WORDファイル(.docx OFFICE 2007以降)
-            //application/zip     Zipファイル
-            //application/x-lzh   LZHファイル
-            //application/x-tar   tarファイル / tar & gzipファイル
             return File(readImage, ret["FiletContentType"].ToString(),ret["Name"].ToString());
         }
 
@@ -86,26 +68,6 @@ namespace CommonwWeb.Controllers
             //データベース読み取り
             Hashtable ret = new Hashtable();
             byte[] readImage = sMssql.ReadImage(2, ref ret);
-
-            //text/plain                 テキストファイル
-            //text/csv                    CSVファイル
-            //text/html                   HTMLファイル
-            //text/css                    CSSファイル
-            //text/javascript             JavaScriptファイル
-            //application/octet-stream    EXEファイルなどの実行ファイル
-            //application/json            JSONファイル
-            //application/pdf             PDFファイル
-            //application/vnd.ms-excel    EXCELファイル( .xls OFFICE 2007より過去)
-            //application/vnd.openxmlformats-officedocument.spreadsheetml.sheet   EXCELファイル( .xlsx OFFICE 2007以降)
-            //application/vnd.ms-powerpoint   PowerPointファイル( .ppt OFFICE 2007より過去)
-            //application/vnd.openxmlformats-officedocument.presentationml.presentation   PowerPointファイル( .pptx OFFICE 2007以降)
-            //application/msword          WORDファイル( .doc OFFICE 2007より過去)
-            //application/vnd.openxmlformats-officedocument.wordprocessingml.document WORDファイル(.docx OFFICE 2007以降)
-            //application/zip     Zipファイル
-            //application/x-lzh   LZHファイル
-            //application/x-tar   tarファイル / tar & gzipファイル
-
-            //var content = new MemoryStream(readImage);
             return new FileContentResult(readImage, ret["FiletContentType"].ToString());
         }
 
@@ -121,28 +83,22 @@ namespace CommonwWeb.Controllers
             //データベース読み取り
             Hashtable ret = new Hashtable();
             byte[] readImage = sMssql.ReadImage(2, ref ret);
-
-            //text/plain                 テキストファイル
-            //text/csv                    CSVファイル
-            //text/html                   HTMLファイル
-            //text/css                    CSSファイル
-            //text/javascript             JavaScriptファイル
-            //application/octet-stream    EXEファイルなどの実行ファイル
-            //application/json            JSONファイル
-            //application/pdf             PDFファイル
-            //application/vnd.ms-excel    EXCELファイル( .xls OFFICE 2007より過去)
-            //application/vnd.openxmlformats-officedocument.spreadsheetml.sheet   EXCELファイル( .xlsx OFFICE 2007以降)
-            //application/vnd.ms-powerpoint   PowerPointファイル( .ppt OFFICE 2007より過去)
-            //application/vnd.openxmlformats-officedocument.presentationml.presentation   PowerPointファイル( .pptx OFFICE 2007以降)
-            //application/msword          WORDファイル( .doc OFFICE 2007より過去)
-            //application/vnd.openxmlformats-officedocument.wordprocessingml.document WORDファイル(.docx OFFICE 2007以降)
-            //application/zip     Zipファイル
-            //application/x-lzh   LZHファイル
-            //application/x-tar   tarファイル / tar & gzipファイル
-
-            //var content = new MemoryStream(readImage);
             HttpContext.Response.Headers.Add("content-disposition", "inline;filename=" + ret["Name"].ToString());
             return new FileContentResult(readImage, ret["FiletContentType"].ToString());
+        }
+
+        /// <summary>
+        /// ダウンロード
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public FileResult Download_Xml()
+        {
+            //データベース読み取り
+            Hashtable ret = new Hashtable();
+            byte[] readImage = sMssql.ReadImage(2, ref ret);
+            return File(readImage, ret["FiletContentType"].ToString());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
