7b7061f39489af847330f3977ee04105f7ea6cbb
Yoshi <h.yoshikazu0110@gmail.com>
2022-08-22 22:58:21 +0900

    ロガーをシングルトンで実装してみる

--- Commons/LoggerBase.cs
+++ Commons/LoggerBase.cs
@@ -5,10 +5,19 @@ using NLog;
 
 namespace Commons
 {
-   public static class LoggerBase
-   {
-      public static string ProcessId = DateTime.Now.ToString("yyyyMMddHHmmssfff"); 
-      
-      public static Logger logger = LogManager.GetCurrentClassLogger();
-   }
+    public static class LoggerBase
+    {
+        public static string ProcessId = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+
+        public static Logger logger = LogManager.GetCurrentClassLogger();
+    }
+
+    public static class LoggerBaseSingle
+    {
+        public static string ProcessId = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static Logger Logger {get {return logger; } }
+    }
 }
